a96a7bfe16ed637a3d69cc94b3c8cc02a5fcd131
Sebastian F <sebpfave2@gmail.com>
2021-01-19 21:20:51 -0500

    README and formatting update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# AppBrew-Adv-JS-and-DOM
+# AppBrewery - Web Development
+
+Course: The Complete Web Development Bootcamp
+Course url: https://www.appbrewery.co/courses
+
+## Section 13 - Advanced Javascript and DOM Manipulation
+
+Drum kit web site application
+
+- Event listeners
+- Play sounds
+- Keyboard event listeners
+- Callbacks
+- Animations
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Detect button press
 for (const button of document.querySelectorAll(".drum")) {
     button.addEventListener("click", function () {
-        
+
         let buttonInnerHTML = this.innerHTML;
         playDrumSound(buttonInnerHTML);
         buttonAnimation(buttonInnerHTML);
-        
-    })    
+
+    })
 }
 
 
@@ -17,7 +17,7 @@ document.addEventListener("keypress", function (event) {
 })
 
 
-function playDrumSound(key){
+function playDrumSound(key) {
     switch (key) {
         case "w":
             let audioCrash = new Audio("sounds/crash.mp3");
@@ -28,12 +28,12 @@ function playDrumSound(key){
             let audioTom1 = new Audio("sounds/tom-1.mp3");
             audioTom1.play();
             break;
-            
+
         case "s":
             let audioTom2 = new Audio("sounds/tom-2.mp3");
             audioTom2.play();
             break;
-    
+
         case "d":
             let audioTom3 = new Audio("sounds/tom-3.mp3");
             audioTom3.play();
@@ -43,26 +43,26 @@ function playDrumSound(key){
             let audioTom4 = new Audio("sounds/tom-4.mp3");
             audioTom4.play();
             break;
-            
+
         case "k":
             let audioKick = new Audio("sounds/kick-bass.mp3");
             audioKick.play();
             break;
-    
+
         case "l":
             let audioSnare = new Audio("sounds/snare.mp3");
             audioSnare.play();
             break;
 
         default:
-            console.log(buttonInnerHTML);
+            console.log("non-programmed key");
     }
 }
 
-function buttonAnimation(key){
+function buttonAnimation(key) {
     let activeButton = document.querySelector(`.${key}`);
     activeButton.classList.add("pressed");
     setTimeout(function () {
         activeButton.classList.remove("pressed");
-    },100);
+    }, 100);
 }
